6128d42dabf51e02afa4e314e2576ca61acdefec
MacRat <m@crat.jp>
2019-12-03 23:07:16 +0900

    Added local cache manager (experimental)

---
+++ lib-landns/local_cache.go
@@ -0,0 +1,185 @@
+package landns
+
+import (
+	"fmt"
+	"time"
+	"sync"
+
+	"github.com/miekg/dns"
+)
+
+type localCacheEntry struct {
+	Record  Record
+	Created time.Time
+	Expire  time.Time
+}
+
+type LocalCache struct {
+	mutex    sync.Mutex
+	entries  map[uint16]map[Domain][]localCacheEntry
+	invoke   chan struct{}
+	closer   chan struct{}
+	upstream Resolver
+}
+
+func NewLocalCache(upstream Resolver) *LocalCache {
+	lc := &LocalCache{
+		entries: make(map[uint16]map[Domain][]localCacheEntry),
+		invoke: make(chan struct{}, 100),
+		closer: make(chan struct{}),
+		upstream: upstream,
+	}
+
+	for _, t := range []uint16{dns.TypeA, dns.TypeNS, dns.TypeCNAME, dns.TypePTR, dns.TypeMX, dns.TypeTXT, dns.TypeAAAA, dns.TypeSRV} {
+		lc.entries[t] = make(map[Domain][]localCacheEntry)
+	}
+
+	go lc.manage()
+
+	return lc
+}
+
+func (lc *LocalCache) String() string {
+	lc.mutex.Lock()
+	defer lc.mutex.Unlock()
+
+	domains := make(map[Domain]struct{})
+	records := 0
+	for _, xs := range lc.entries {
+		for name, x := range xs {
+			domains[name] = struct{}{}
+			records += len(x)
+		}
+	}
+
+	return fmt.Sprintf("LocalCache[%d domains %d records]", len(domains), records)
+}
+
+func (lc *LocalCache) Close() {
+	close(lc.closer)
+	close(lc.invoke)
+}
+
+func (lc *LocalCache) manageTask() (next time.Duration) {
+	next = 10*time.Second
+
+	lc.mutex.Lock()
+	defer lc.mutex.Unlock()
+
+	for _, domains := range lc.entries {
+		for name, entries := range domains {
+			sweep := false
+
+			for _, entry := range entries {
+				delta := entry.Expire.Sub(time.Now())
+				if delta < 1 {
+					sweep = true
+					break
+				} else if next > delta {
+					next = delta
+				}
+			}
+
+			if sweep {
+				delete(domains, name)
+			}
+		}
+	}
+
+	return next
+}
+
+func (lc *LocalCache) manage() {
+	for {
+		lc.manageTask()
+
+		select {
+		case <-time.After(lc.manageTask()):
+		case <-lc.invoke:
+		case <-lc.closer:
+			return
+		}
+	}
+}
+
+func (lc *LocalCache) add(r Record) {
+	if r.GetTTL() == 0 {
+		return
+	}
+
+	if _, ok := lc.entries[r.GetQtype()][r.GetName()]; !ok {
+		lc.entries[r.GetQtype()][r.GetName()] = []localCacheEntry{
+			{
+				Record: r,
+				Created: time.Now(),
+				Expire: time.Now().Add(time.Duration(r.GetTTL()) * time.Second),
+			},
+		}
+	} else {
+		lc.entries[r.GetQtype()][r.GetName()] = append(lc.entries[r.GetQtype()][r.GetName()], localCacheEntry{
+			Record: r,
+			Created: time.Now(),
+			Expire: time.Now().Add(time.Duration(r.GetTTL()) * time.Second),
+		})
+	}
+
+	lc.invoke <- struct{}{}
+}
+
+func (lc *LocalCache) resolveFromUpstream(w ResponseWriter, r Request) error {
+	wh := ResponseWriterHook{
+		Writer: w,
+		OnAdd: lc.add,
+	}
+
+	return lc.upstream.Resolve(wh, r)
+}
+
+func (lc *LocalCache) resolveFromCache(w ResponseWriter, records []localCacheEntry) error {
+	w.SetNoAuthoritative()
+
+	now := time.Now()
+
+	for _, cache := range records {
+		rr, err := cache.Record.ToRR()
+		if err != nil {
+			return err
+		}
+
+		rr.Header().Ttl -= uint32(now.Sub(cache.Created).Seconds())
+
+		record, err := NewRecordFromRR(rr)
+		if err != nil {
+			return err
+		}
+
+		if err := w.Add(record); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (lc *LocalCache) Resolve(w ResponseWriter, r Request) error {
+	lc.mutex.Lock()
+	defer lc.mutex.Unlock()
+
+	records, ok := lc.entries[r.Qtype][Domain(r.Name)]
+	if !ok {
+		return lc.resolveFromUpstream(w, r)
+	}
+
+	for _, cache := range records {
+		if cache.Expire.Sub(time.Now()) < 1 {
+			delete(lc.entries[r.Qtype], Domain(r.Name))
+			return lc.resolveFromUpstream(w, r)
+		}
+	}
+
+	return lc.resolveFromCache(w, records)
+}
+
+func (lc *LocalCache) RecursionAvailable() bool {
+	return lc.upstream.RecursionAvailable()
+}
---
+++ lib-landns/local_cache_test.go
@@ -0,0 +1,85 @@
+package landns_test
+
+import (
+	"testing"
+	"net"
+	"time"
+
+	"github.com/miekg/dns"
+	"github.com/macrat/landns/lib-landns"
+)
+
+func TestLocalCache(t *testing.T) {
+	upstream := landns.NewSimpleResolver(
+		[]landns.Record{
+			landns.AddressRecord{Name: landns.Domain("example.com."), TTL: 100, Address: net.ParseIP("127.1.2.3")},
+			landns.AddressRecord{Name: landns.Domain("example.com."), TTL: 10, Address: net.ParseIP("127.2.3.4")},
+			landns.AddressRecord{Name: landns.Domain("short.example.com."), TTL: 1, Address: net.ParseIP("127.3.4.5")},
+			landns.AddressRecord{Name: landns.Domain("no-cache.example.com."), TTL: 0, Address: net.ParseIP("127.4.5.6")},
+		},
+	)
+	if err := upstream.Validate(); err != nil {
+		t.Fatalf("failed to validate upstream resolver: %s", err)
+	}
+	resolver := landns.NewLocalCache(upstream)
+	defer resolver.Close()
+
+	if resolver.String() != "LocalCache[0 domains 0 records]" {
+		t.Errorf("unexpected string: %s", resolver)
+	}
+
+	ResolverTest(t, upstream, landns.NewRequest("example.com.", dns.TypeA, false), true, "example.com. 100 IN A 127.1.2.3", "example.com. 10 IN A 127.2.3.4")
+
+	tests := []func(chan struct{}){
+		func(ch chan struct{}) {
+			ResolverTest(t, resolver, landns.NewRequest("example.com.", dns.TypeA, false), true, "example.com. 100 IN A 127.1.2.3", "example.com. 10 IN A 127.2.3.4")
+			time.Sleep(1 * time.Second)
+			ResolverTest(t, resolver, landns.NewRequest("example.com.", dns.TypeA, false), false, "example.com. 99 IN A 127.1.2.3", "example.com. 9 IN A 127.2.3.4")
+			close(ch)
+		},
+		func(ch chan struct{}) {
+			ResolverTest(t, resolver, landns.NewRequest("short.example.com.", dns.TypeA, false), true, "short.example.com. 1 IN A 127.3.4.5")
+			time.Sleep(100 * time.Millisecond)
+			ResolverTest(t, resolver, landns.NewRequest("short.example.com.", dns.TypeA, false), false, "short.example.com. 1 IN A 127.3.4.5")
+			time.Sleep(1 * time.Second)
+			ResolverTest(t, resolver, landns.NewRequest("short.example.com.", dns.TypeA, false), true, "short.example.com. 1 IN A 127.3.4.5")
+			close(ch)
+		},
+		func(ch chan struct{}) {
+			ResolverTest(t, resolver, landns.NewRequest("no-cache.example.com.", dns.TypeA, false), true, "no-cache.example.com. 0 IN A 127.4.5.6")
+			ResolverTest(t, resolver, landns.NewRequest("no-cache.example.com.", dns.TypeA, false), true, "no-cache.example.com. 0 IN A 127.4.5.6")
+			close(ch)
+		},
+	}
+	waits := make([]chan struct{}, len(tests))
+	for i, test := range tests {
+		waits[i] = make(chan struct{})
+		go test(waits[i])
+	}
+	for _, ch := range waits {
+		<-ch
+	}
+
+	if resolver.String() != "LocalCache[2 domains 3 records]" {
+		t.Errorf("unexpected string: %s", resolver)
+	}
+}
+
+func BenchmarkLocalCache(b *testing.B) {
+	upstream := landns.NewSimpleResolver([]landns.Record{
+		landns.AddressRecord{Name: landns.Domain("example.com."), TTL: 100, Address: net.ParseIP("127.1.2.3")},
+	})
+	if err := upstream.Validate(); err != nil {
+		b.Fatalf("failed to validate upstream resolver: %s", err)
+	}
+	resolver := landns.NewLocalCache(upstream)
+	defer resolver.Close()
+
+	req := landns.NewRequest("example.com.", dns.TypeA, false)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		resolver.Resolve(NewDummyResponseWriter(), req)
+	}
+}
