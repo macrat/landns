dac39c425be0a2253eebfe900dd65a55f87451ce
MacRat <m@crat.jp>
2019-12-14 16:40:01 +0900

    Added DynamicRecord struct

--- lib-landns/dynamic_resolver.go
+++ lib-landns/dynamic_resolver.go
@@ -1,5 +1,110 @@
 package landns
 
+import (
+	"strconv"
+	"bytes"
+	"fmt"
+
+	"github.com/miekg/dns"
+)
+
+var (
+	ErrMultiLineDynamicRecord = fmt.Errorf("DynamicRecord can't have multi line")
+	ErrInvalidDynamicRecordFormat = fmt.Errorf("DynamicRecord invalid format")
+)
+
+type DynamicRecord struct {
+	Record Record
+	ID     *int
+}
+
+func (r DynamicRecord) String() string {
+	if r.ID == nil {
+		return r.Record.String()
+	}
+	return r.Record.String() + " ; ID:" + strconv.Itoa(*r.ID)
+}
+
+func (r *DynamicRecord) UnmarshalText(text []byte) error {
+	if bytes.Contains(text, []byte("\n")) {
+		return ErrMultiLineDynamicRecord
+	}
+
+	xs := bytes.SplitN(text, []byte(";"), 2)
+	if len(xs) != 2 {
+		r.ID = nil
+	} else {
+		for _, x := range bytes.Split(bytes.TrimSpace(xs[1]), []byte(" ")) {
+			kvs := bytes.SplitN(x, []byte(":"), 2)
+			if len(kvs) != 2 {
+				return ErrInvalidDynamicRecordFormat
+			}
+
+			switch string(bytes.TrimSpace(kvs[0])) {
+			case "ID":
+				id, err := strconv.Atoi(string(bytes.TrimSpace(kvs[1])))
+				if err != nil {
+					return ErrInvalidDynamicRecordFormat
+				}
+				r.ID = &id
+			}
+		}
+	}
+
+	rr, err := dns.NewRR(string(xs[0]))
+	if err != nil {
+		return err
+	}
+
+	r.Record, err = NewRecordFromRR(rr)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r DynamicRecord) MarshalText() ([]byte, error) {
+	return []byte(r.String()), nil
+}
+
+type DynamicRecordSet []DynamicRecord
+
+func (rs *DynamicRecordSet) UnmarshalText(text []byte) error {
+	lines := bytes.Split(text, []byte("\n"))
+
+	*rs = make([]DynamicRecord, 0, len(lines))
+
+	for _, line := range lines {
+		line = bytes.TrimSpace(line)
+		if len(line) == 0 || line[0] == ';' {
+			continue
+		}
+
+		var r DynamicRecord
+		if err := (&r).UnmarshalText(line); err != nil {
+			return err
+		}
+		*rs = append(*rs, r)
+	}
+
+	return nil
+}
+
+func (rs DynamicRecordSet) MarshalText() ([]byte, error) {
+	var err error
+
+	bs := make([][]byte, len(rs)+1)  // add +1 element for put \n into last line
+	for i, r := range rs {
+		bs[i], err = r.MarshalText()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return bytes.Join(bs, []byte("\n")), nil
+}
+
 type DynamicResolver interface {
 	Resolver
 
@@ -14,4 +119,7 @@ type DynamicResolver interface {
 
 	UpdateServices(ServicesConfig) error
 	GetServices() (ServicesConfig, error)
+
+	//SetRecords(DynamicRecordSet) error
+	//Records() (DynamicRecordSet, error)
 }
---
+++ lib-landns/dynamic_resolver_test.go
@@ -0,0 +1,85 @@
+package landns_test
+
+import (
+	"testing"
+
+	"github.com/macrat/landns/lib-landns"
+)
+
+func TestDynamicRecord(t *testing.T) {
+	tests := []struct{
+		Input  string
+		Expect string
+		Error  string
+	}{
+		{"example.com. 42 IN A 127.0.1.2 ; ID:123", "example.com. 42 IN A 127.0.1.2 ; ID:123", ""},
+		{"example.com. 123 IN A 127.3.4.5 ; dummy:aa ID:67 foo:bar", "example.com. 123 IN A 127.3.4.5 ; ID:67", ""},
+		{"hello 100 IN A 127.6.7.8", "hello. 100 IN A 127.6.7.8", ""},
+		{"v6.example.com. 321 IN AAAA 4::2", "v6.example.com. 321 IN AAAA 4::2", ""},
+		{"example.com.\t135\tIN\tTXT\thello\t;\tID:1", "example.com. 135 IN TXT \"hello\" ; ID:1", ""},
+		{"c.example.com. IN CNAME example.com. ; ID:2", "c.example.com. 3600 IN CNAME example.com. ; ID:2", ""},
+		{"_web._tcp.example.com. SRV 1 2 3 example.com. ; ID:4", "_web._tcp.example.com. 3600 IN SRV 1 2 3 example.com. ; ID:4", ""},
+		{"2.1.0.127.in-arpa.addr. 2 IN PTR example.com. ; ID:987654321", "2.1.0.127.in-arpa.addr. 2 IN PTR example.com. ; ID:987654321", ""},
+		{"a\nb", "", landns.ErrMultiLineDynamicRecord.Error()},
+		{"example.com. 42 IN A 127.0.1.2 ; ID", "", landns.ErrInvalidDynamicRecordFormat.Error()},
+		{"example.com. 42 IN A 127.0.1.2 ; ID: 42", "", landns.ErrInvalidDynamicRecordFormat.Error()},
+		{"example.com. 42 IN A 127.0.1.2 ; ID:foobar", "", landns.ErrInvalidDynamicRecordFormat.Error()},
+		{"hello world ; ID:1", "", `dns: not a TTL: "world" at line: 1:12`},
+	}
+
+	var r landns.DynamicRecord
+
+	for _, tt := range tests {
+		if err := (&r).UnmarshalText([]byte(tt.Input)); err != nil && tt.Error == "" {
+			t.Errorf("failed to unmarshal dynamic record: %v", err)
+			continue
+		} else if err != nil && err.Error() != tt.Error {
+			t.Errorf(`unmarshal dynamic record: expected error "%v" but got "%v"`, tt.Error, err)
+			continue
+		}
+		if tt.Error != "" {
+			continue
+		}
+
+		if got, err := r.MarshalText(); err != nil {
+			t.Errorf("failed to marshal dynamic record: %v", err)
+		} else if string(got) != tt.Expect {
+			t.Errorf("encoded text was unexpected:\n\texpected: %#v\n\tbut got:  %#v", tt.Expect, string(got))
+		}
+	}
+}
+
+func TestDynamicRecordSet(t *testing.T) {
+	tests := []struct{
+		Input  string
+		Expect string
+		Error  string
+	}{
+		{"example.com. 42 IN A 127.0.1.2 ; ID:3", "example.com. 42 IN A 127.0.1.2 ; ID:3\n", ""},
+		{"example.com. 42 IN A 127.0.1.2 ; ID:3\nexample.com. 24 IN AAAA 1:2:3::4", "example.com. 42 IN A 127.0.1.2 ; ID:3\nexample.com. 24 IN AAAA 1:2:3::4\n", ""},
+		{"\n\n\nexample.com. 42 IN A 127.0.1.2 ; ID:3\n\n", "example.com. 42 IN A 127.0.1.2 ; ID:3\n", ""},
+		{";this\n  ;is\n\t; comment", "", ""},
+		{"unexpected", "", `dns: not a TTL: "unexpected" at line: 1:10`},
+	}
+
+	var rs landns.DynamicRecordSet
+
+	for _, tt := range tests {
+		if err := (&rs).UnmarshalText([]byte(tt.Input)); err != nil && tt.Error == "" {
+			t.Errorf("failed to unmarshal dynamic record set: %v", err)
+			continue
+		} else if err != nil && err.Error() != tt.Error {
+			t.Errorf(`unmarshal dynamic record set: expected error "%v" but got "%v"`, tt.Error, err)
+			continue
+		}
+		if tt.Error != "" {
+			continue
+		}
+
+		if got, err := rs.MarshalText(); err != nil {
+			t.Errorf("failed to marshal dynamic record set: %v", err)
+		} else if string(got) != tt.Expect {
+			t.Errorf("encoded text was unexpected:\n\texpected: %#v\n\tbut got:  %#v", tt.Expect, string(got))
+		}
+	}
+}
