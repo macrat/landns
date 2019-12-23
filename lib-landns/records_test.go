5da7ba8e66a187b842974223956d105226c43314
MacRat <m@crat.jp>
2019-12-23 22:40:14 +0900

    Added simple tests for records

--- lib-landns/records_test.go
+++ lib-landns/records_test.go
@@ -2,6 +2,7 @@ package landns_test
 
 import (
 	"fmt"
+	"net"
 	"testing"
 
 	"github.com/macrat/landns/lib-landns"
@@ -67,6 +68,95 @@ func TestDomain_ToPath(t *testing.T) {
 	for _, tt := range tests {
 		if p := tt.Input.ToPath(); p != tt.Expect {
 			t.Errorf("unexpected path:\nexpected: %s\nbut got:  %s", tt.Expect, p)
+		}
+	}
+}
+
+func TestRecords(t *testing.T) {
+	tests := []struct {
+		Record landns.Record
+		String string
+		Qtype  uint16
+		TTL    uint32
+	}{
+		{
+			landns.AddressRecord{Name: "a.example.com.", TTL: 10, Address: net.ParseIP("127.0.0.1")},
+			"a.example.com. 10 IN A 127.0.0.1",
+			dns.TypeA,
+			10,
+		},
+		{
+			landns.AddressRecord{Name: "aaaa.example.com.", TTL: 20, Address: net.ParseIP("4::2")},
+			"aaaa.example.com. 20 IN AAAA 4::2",
+			dns.TypeAAAA,
+			20,
+		},
+		{
+			landns.NsRecord{Name: "ns.example.com.", Target: "example.com."},
+			"ns.example.com. IN NS example.com.",
+			dns.TypeNS,
+			0,
+		},
+		{
+			landns.CnameRecord{Name: "cname.example.com.", TTL: 40, Target: "example.com."},
+			"cname.example.com. 40 IN CNAME example.com.",
+			dns.TypeCNAME,
+			40,
+		},
+		{
+			landns.PtrRecord{Name: "1.0.0.127.in-addr.arpa.", TTL: 50, Domain: "ptr.example.com."},
+			"1.0.0.127.in-addr.arpa. 50 IN PTR ptr.example.com.",
+			dns.TypePTR,
+			50,
+		},
+		{
+			landns.MxRecord{Name: "mx.example.com.", TTL: 60, Preference: 42, Target: "example.com."},
+			"mx.example.com. 60 IN MX 42 example.com.",
+			dns.TypeMX,
+			60,
+		},
+		{
+			landns.TxtRecord{Name: "txt.example.com.", TTL: 70, Text: "hello world"},
+			"txt.example.com. 70 IN TXT \"hello world\"",
+			dns.TypeTXT,
+			70,
+		},
+		{
+			landns.SrvRecord{Name: "_web._tcp.srv.example.com.", TTL: 80, Priority: 11, Weight: 22, Port: 33, Target: "example.com."},
+			"_web._tcp.srv.example.com. 80 IN SRV 11 22 33 example.com.",
+			dns.TypeSRV,
+			80,
+		},
+	}
+
+	for _, tt := range tests {
+		if err := tt.Record.Validate(); err != nil {
+			t.Errorf("failed to validate: %s", err)
+		}
+		if s := tt.Record.String(); s != tt.String {
+			t.Errorf("failed to convert to string:\nexpected: %s\nbut got:  %s", tt.String, s)
+		}
+		if q := tt.Record.GetQtype(); q != tt.Qtype {
+			t.Errorf("unexpected qtype: expected %d but got %d", tt.Qtype, q)
+		}
+		if ttl := tt.Record.GetTTL(); ttl != tt.TTL {
+			t.Errorf("unexpected ttl: expected %d but got %d", tt.TTL, ttl)
+		}
+
+		rr1, err := tt.Record.ToRR()
+		if err != nil {
+			t.Errorf("failed to convert to dns.RR: %s", err)
+			continue
+		}
+
+		rr2, err := dns.NewRR(tt.String)
+		if err != nil {
+			t.Errorf("failed to convert example to dns.RR: %s", err)
+			continue
+		}
+
+		if rr1.String() != rr2.String() {
+			t.Errorf("unexpected RR:\nexpected: %s\nbut got:  %s", rr2, rr1)
 		}
 	}
 }
