da6b8ce5d523fb4fe20eb2f98cf3fd2f4e34b13c
MacRat <m@crat.jp>
2019-12-23 20:37:58 +0900

    Replaced wait channels with WaitGroup

--- lib-landns/cache_test.go
+++ lib-landns/cache_test.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"testing"
 	"time"
+	"sync"
 
 	"github.com/macrat/landns/lib-landns"
 	"github.com/miekg/dns"
@@ -30,39 +31,36 @@ func CacheTestUpstream(t testing.TB) landns.Resolver {
 }
 
 func CacheTest(t *testing.T, resolver landns.Resolver) {
-	tests := []func(chan struct{}){
-		func(ch chan struct{}) {
+	tests := []func(){
+		func() {
 			AssertResolve(t, resolver, landns.NewRequest("example.com.", dns.TypeA, false), true, "example.com. 100 IN A 127.1.2.3", "example.com. 10 IN A 127.2.3.4")
 			time.Sleep(1 * time.Second)
 			AssertResolve(t, resolver, landns.NewRequest("example.com.", dns.TypeA, false), false, "example.com. 99 IN A 127.1.2.3", "example.com. 9 IN A 127.2.3.4")
-			close(ch)
 		},
-		func(ch chan struct{}) {
+		func() {
 			AssertResolve(t, resolver, landns.NewRequest("short.example.com.", dns.TypeA, false), true, "short.example.com. 10 IN A 127.3.4.5", "short.example.com. 2 IN A 127.4.5.6")
 			time.Sleep(1 * time.Second)
 			AssertResolve(t, resolver, landns.NewRequest("short.example.com.", dns.TypeA, false), false, "short.example.com. 9 IN A 127.3.4.5", "short.example.com. 1 IN A 127.4.5.6")
 			time.Sleep(1200 * time.Millisecond)
 			AssertResolve(t, resolver, landns.NewRequest("short.example.com.", dns.TypeA, false), true, "short.example.com. 10 IN A 127.3.4.5", "short.example.com. 2 IN A 127.4.5.6")
-			close(ch)
 		},
-		func(ch chan struct{}) {
+		func() {
 			AssertResolve(t, resolver, landns.NewRequest("no-cache.example.com.", dns.TypeA, false), true, "no-cache.example.com. 0 IN A 127.5.6.7")
 			AssertResolve(t, resolver, landns.NewRequest("no-cache.example.com.", dns.TypeA, false), true, "no-cache.example.com. 0 IN A 127.5.6.7")
-			close(ch)
 		},
-		func(ch chan struct{}) {
+		func() {
 			AssertResolve(t, resolver, landns.NewRequest("example.com.", dns.TypeTXT, false), true, "example.com. 100 IN TXT \"hello world\"")
 			time.Sleep(1 * time.Second)
 			AssertResolve(t, resolver, landns.NewRequest("example.com.", dns.TypeTXT, false), false, "example.com. 99 IN TXT \"hello world\"")
-			close(ch)
 		},
 	}
-	waits := make([]chan struct{}, len(tests))
-	for i, test := range tests {
-		waits[i] = make(chan struct{})
-		go test(waits[i])
+	wg := sync.WaitGroup{}
+	for _, test := range tests {
+		wg.Add(1)
+		go func(t func()) {
+			t()
+			wg.Done()
+		}(test)
 	}
-	for _, ch := range waits {
-		<-ch
-	}
+	wg.Wait()
 }
